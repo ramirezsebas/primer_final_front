8f704c19e39d96bfbee346d3335f85b7308be810
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-25 18:21:23 -0300

    feat: se agrega el side nav a la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ProductosModule } from './features/productos/productos.module';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +20,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // Modulos de Material
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+
+    // Modulos de la app
+    ProductosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
