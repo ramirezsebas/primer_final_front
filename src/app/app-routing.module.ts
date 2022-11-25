4fe264759074552db0eeb26f88a41e767f06fde0
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-25 18:21:35 -0300

    Agregamos la ruta de productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductosPageComponent } from './features/productos/pages/productos-page/productos-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    component: ProductosPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
