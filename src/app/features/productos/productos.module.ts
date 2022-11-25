f20e7ef0f77ecd3e67ea8d86d7583c60f458732f
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-25 19:03:47 -0300

    feat: Importamos modulos necesarios para el formulario y material

--- src/app/features/productos/productos.module.ts
+++ src/app/features/productos/productos.module.ts
@@ -4,20 +4,34 @@ import { ProductosPageComponent } from './pages/productos-page/productos-page.co
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
+import { ProductoDialogComponent } from './components/producto-dialog/producto-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-
-    ProductosPageComponent
+    ProductosPageComponent,
+    ProductoDialogComponent
   ],
   imports: [
     CommonModule,
-
+    ReactiveFormsModule,
     MatIconModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule
+
 
   ]
 })
